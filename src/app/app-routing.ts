2874242b8680ce3dff427dc3ac14a6f19dbfa786
Fehér Marcell <78973983+fmarcell21@users.noreply.github.com>
2023-03-17 08:07:38 +0100

    Initial commit

---
+++ src/app/app-routing.ts
@@ -0,0 +1,14 @@
+import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
+import {NgModule} from "@angular/core";
+
+const appRoutes: Routes = [
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import {HttpClientModule} from "@angular/common/http";
+
+import * as fromApp from "./State/app.reducer"
+import {AppRoutingModule} from "./app-routing";
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot(fromApp.appReducer),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
